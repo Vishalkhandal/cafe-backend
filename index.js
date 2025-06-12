ac73b249d4c262d4ac77dfb5b48db9982be095b5
vishalkhandal <vishalkhandal96@gmail.com>
2025-06-12 15:37:48 +0530

    feat: userRoutes and cors origin add

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require('cors');
 const connectDB = require('./db');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 const port = 3000;
 
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(cors({origin: 'http://localhost:5173'}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
