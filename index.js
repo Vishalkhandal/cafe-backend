a1b4fd8eaacd8cb9b9ab15a97c4b02d12713e7db
vishalkhandal <vishalkhandal96@gmail.com>
2025-06-12 15:23:21 +0530

    feat: add use schema

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require('cors');
+const connectDB = require('./db');
 
 const app = express();
 const port = 3000;
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
